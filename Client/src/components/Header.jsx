512aaeadf5973ccd19231e54389eccd68ed8218c
PrittoRuban <prittoruban0609@gmail.com>
2024-04-08 18:12:47 +0530

    create and run the server

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -40,7 +40,7 @@ export default function App() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
