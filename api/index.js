caa8471aaf35f6ef86595648da9e06908396ea18
PrittoRuban <prittoruban0609@gmail.com>
2024-04-09 13:23:01 +0530

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("MongoDB is connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
-});
+})
