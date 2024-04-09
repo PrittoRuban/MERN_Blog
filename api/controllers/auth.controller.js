c63ec351291db3d30706f8b5c501389bc0468f6b
PrittoRuban <prittoruban0609@gmail.com>
2024-04-09 15:07:30 +0530

    Create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,33 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+
+
+  const hashPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json({ message: "User created successfully!" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
