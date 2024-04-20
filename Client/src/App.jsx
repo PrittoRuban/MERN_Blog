efbdf03a30aaf04c68f559b66256a705a5bb96e8
PrittoRuban <prittoruban0609@gmail.com>
2024-04-20 13:17:20 +0530

    Create and add footer component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
@@ -8,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -22,6 +22,8 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,98 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsMedium, BsPinterest, BsReddit, BsStackOverflow, BsTwitterX} from "react-icons/bs";
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg
+       sm:text-xl font-semibold dark:text-white"
+            >
+              <span
+                className="px-2 py -1 bg-gradient-to-r from-indigo-500
+         via-purple-500 to-pink-500 rounded-lg text-white"
+              >
+                Pritto's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div
+            className="grid grid-cols-2 gap-8 mt-4  sm:grid-cols-3
+          sm:gap-6"
+          >
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/PrittoRuban?tab=repositories"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  My Projects
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.reddit.com/user/prittoruban/?rdt=32844"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Reddit
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Get Connected" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/PrittoRuban"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </Footer.Link>
+                <Footer.Link
+                  href="https://linkedin.com/in/prittoruban"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Pritto Ruban's Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-3 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="https://github.com/PrittoRuban" target="_blank" icon={BsGithub} />
+            <Footer.Icon href="https://twitter.com/prittoruban" target="_blank" icon={BsTwitterX} />
+            <Footer.Icon href="https://linkedin.com/in/prittoruban" target="_blank" icon={BsLinkedin} />
+            <Footer.Icon href="https://www.facebook.com/prittoruban?mibextid=ZbWKwL" target="_blank" icon={BsFacebook} />
+            <Footer.Icon href="https://www.instagram.com/prittorubanofficial/?igsh=ODhxZjQ5YW5nbG9h" target="_blank" icon={BsInstagram} />
+            <Footer.Icon href="https://www.reddit.com/user/prittoruban/?rdt=32844" target="_blank" icon={BsReddit} />
+            <Footer.Icon href="https://pin.it/5OsHuu85T" target="_blank" icon={BsPinterest} />
+            <Footer.Icon href="https://medium.com/@prittoruban" target="_blank" icon={BsMedium} />
+            <Footer.Icon href="https://stackoverflow.com/users/22302751/pritto-ruban" target="_blank" icon={BsStackOverflow} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
