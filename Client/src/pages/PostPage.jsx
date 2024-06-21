ff9891fb4e8d032689093ca641b05f8ef7ade1d7
PrittoRuban <prittoruban0609@gmail.com>
2024-06-21 15:28:41 +0530

    update post page

--- Client/src/pages/PostPage.jsx
+++ Client/src/pages/PostPage.jsx
@@ -70,7 +70,7 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className="">
+      <div className="mt-8">
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
